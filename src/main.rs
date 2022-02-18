82c17f761db2655b6815aae85bec10ab5da6686c
Niklas <36010519+neon-js@users.noreply.github.com>
2022-02-18 18:23:08 +0100

    🖥️ Better CLI argument handling & variable width
    - Using `clap` to provide more CLI arguments
    - Allow giving height via CLI

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
-use std::env;
 use std::fs::File;
 use std::io::{BufReader};
 use std::ops::Add;
 use crate::colors::rgb;
 use crate::flag::{Flag};
+use clap::{arg, Command};
 
 mod colors;
 mod flag;
 
 fn main() {
-    let information = information_from_args();
-    if information.is_none() {
-        eprintln!("Error: Wrong usage.");
-        eprintln!("       Call ./ironic_flags <FLAGS_FILE> <FLAG_NAME>");
+    let args = Command::new("ironic_flags")
+        .version( "0.1.0")
+        .author("Niklas Schmidt <36010519+neon-js@users.noreply.github.com>")
+        .about("Shows colored flags in ansi terminals.")
+        .arg(
+            arg!(-f --file <FILE> "File that contains the flag definitions")
+        )
+        .arg(
+            arg!(-n --flag <FLAGNAME> "Name of flag to show")
+        )
+        .arg(
+            arg!(-w --width [WIDTH] "Width of flag. If omitted, it's set to ~ height * 2.")
+        )
+        .arg(
+            arg!(-h --height [HEIGHT] "Height of flag. If omitted, it's set the minimal value that's necessary to show the flag")
+        )
+        .get_matches();
+
+    let file_name = args.value_of("file")
+        .expect("Argument file is required!");
+
+    let flag_name = args.value_of("flag")
+        .expect("Argument flag-name is required!");
+
+    let height = args.value_of("height")
+        .or(Some("0"))
+        .and_then(|value|value.parse::<usize>().ok());
+
+    let width = args.value_of("width")
+        .and_then(|value|value.parse::<usize>().ok());
+
+    if height.is_none() {
+        eprintln!("Error: Invalid height given.");
         return;
     }
-
-    let (file_name, flag_name) = information.unwrap();
 
     let file_reader = File::open(&file_name)
         .map(|file| BufReader::new(file));
@@ -45,8 +72,7 @@ fn main() {
         return;
     }
 
-    /* TODO: Make height and width variable */
-    println!("{}", generate_flag_text(flag.unwrap(), 5));
+    println!("{}", generate_flag_text(flag.unwrap(), height.unwrap(), width));
 }
 
 /// Generates text that shows given flag in an ansi terminal.
@@ -55,7 +81,8 @@ fn main() {
 ///
 /// * `flag` - Flag that should be displayed
 /// * `height` - _minimal_ height of the flag to display
-fn generate_flag_text(flag: &Flag, height: usize) -> String
+/// * `width` - If given, width of the flag to display. If omitted, its set to ~ `2 * height`.
+fn generate_flag_text(flag: &Flag, height: usize, width: Option<usize>) -> String
 {
     let relation_sum = flag.parts.iter()
         .map(|part| part.relation)
@@ -67,7 +94,7 @@ fn generate_flag_text(flag: &Flag, height: usize) -> String
         height_factor = 1.0;
     }
 
-    let width = relation_sum * 4;
+    let width = width.unwrap_or(relation_sum * (height_factor as usize) * 4);
 
     flag.parts.iter()
         .map(|part| {
@@ -76,12 +103,4 @@ fn generate_flag_text(flag: &Flag, height: usize) -> String
         })
         .collect::<String>()
         .add(colors::reset())
-}
-
-fn information_from_args() -> Option<(String, String)>
-{
-    let mut args = env::args();
-    args.next()?; /* Program name */
-
-    Some((args.next()?, args.next()?))
 }
